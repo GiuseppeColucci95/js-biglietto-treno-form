1278b9e7090212f5f27c6d4793574ae6e255d60c
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-19 15:14:49 +0100

    add milestone 1

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-//ask to the user the number of km to do and save it in a variable
+//get node of the page and save it in a variable
+const distanceEl = document.getElementById('distance');
+console.log(distanceEl);
 
-//ask to the user his age and save it in a variable
+const ageEl = document.getElementById('age');
+console.log(ageEl);
 
-//calculate the ticket price and save it in a variable
+const submitEl = document.querySelector('button');
+console.log(submitEl);
 
-//calculate optional discount if minors of 18 years old
+//add event listener to the submit button
+submitEl.addEventListener('click', function () {
 
-//calculate optional discount if over 65 years old
+  //get the number of km to do and save it in a variable
+  const km = distanceEl.value;
+  console.log("Km: " + km);
+
+  //get the passenger age and save it in a variable
+  const age = ageEl.value;
+  console.log("Age: " + age);
+
+  //calculate the ticket price and save it in a variable
+  const priceForKm = 0.21;
+  const minorDiscount = 0.20;
+  const seniorDiscount = 0.40;
+  let totalPrice = priceForKm * km;
+  console.log("Price not discounted: " + totalPrice.toFixed(2));
+
+  //calculate optional discount if minors of 18 years old
+  if (age < 18) {
+    totalPrice -= totalPrice * minorDiscount;
+  } else if (age >= 65) {
+    //calculate optional discount if over 65 years old
+    totalPrice -= totalPrice * seniorDiscount;
+  }
+
+  console.log("Final price: " + totalPrice.toFixed(2));
+
+
+});
+

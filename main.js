9631215fae63e6a60fa1308a70fe2fc9ed08faa8
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-19 14:54:40 +0100

    add initial structure for milestone 1
    -added initial structure in html
    -added human code in js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("works");
+//ask to the user the number of km to do and save it in a variable
+
+//ask to the user his age and save it in a variable
+
+//calculate the ticket price and save it in a variable
+
+//calculate optional discount if minors of 18 years old
+
+//calculate optional discount if over 65 years old
